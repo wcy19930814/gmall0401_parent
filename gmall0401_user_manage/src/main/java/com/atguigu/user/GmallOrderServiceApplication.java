a854a0e63a9cba087283860288f12fc0126c0db3
weichaoying <1176243848@qq.com>
2019-09-04 16:51:54 +0800

    电商git小测试01

---
+++ gmall0401_user_manage/src/main/java/com/atguigu/user/GmallOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("")
+public class GmallOrderServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GmallOrderServiceApplication.class,args);
+    }
+}
