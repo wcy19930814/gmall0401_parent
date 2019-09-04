838693ebbbfc60a0319516564e3acf4c7cba0915
weichaoying <1176243848@qq.com>
2019-09-04 16:41:47 +0800

    电商git小测试

---
+++ src/main/java/com/atguigu/gmall/parent/Gmall0401ParentApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0401ParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gmall0401ParentApplication.class, args);
+	}
+
+}
